5b6e6c5ce62d00930dd0efce9735ea08dadee799
pdcdeveloper <pdcdeveloper@outlook.com>
2018-02-15 14:28:41 -0800

    Add AttributeHelper and DisplayAttributeExtension classes

---
+++ pqlib/Reflection/Attributes/AttributeHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pqlib.Reflection.Attributes
+{
+    /// <summary>
+    /// The methods in this class can be used to retrieve the attributes
+    /// </summary>
+    /// <remarks>
+    /// Assigning default values to generic types:
+    ///     <see cref="https://msdn.microsoft.com/en-us/library/xwth0h0d.aspx"/>
+    /// </remarks>
+    public class AttributeHelper
+    {
+        public FieldInfo GetFieldInfo<T>(T member)
+        {
+            return member.GetType().GetField(nameof(member));
+        }
+
+        public IEnumerable<MemberInfo> GetMemberInfo<T>(T member)
+        {
+            MemberInfo[] info = member.GetType().GetMember(member.ToString());
+            if ((info?.Length ?? 0) > 0)
+                return info.AsEnumerable();
+            return null;
+        }
+
+        public bool TryGetSpecifiedAttribute<T1, T2>(T1 member, out T2 attr)
+            where T2 : Attribute
+        {
+            var info = GetMemberInfo(member);
+            if ((info?.Count() ?? 0) > 0)
+                foreach (var m in info)
+                {
+                    var attributes = m.GetCustomAttributes(typeof(T2), false);
+                    if ((attributes?.Count() ?? 0) > 0)
+                        foreach (var a in attributes)
+                            if (a is T2)
+                            {
+                                attr = a as T2;
+                                return true;
+                            }
+                }
+            attr = null;//attr = default(T2);
+            return false;
+        }
+
+        public PropertyInfo GetPropertyInfo<T>(T member)
+        {
+            return member.GetType().GetProperty(nameof(member));
+        }
+    }
+}
---
+++ pqlib/Reflection/Attributes/DisplayAttributeExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pqlib.Reflection.Attributes
+{
+    // The extension methods from this class is typically used on enum members that use 'DataAnnotations.DisplayAttribute'.
+    public static class DisplayAttributeExtension
+    {
+        static AttributeHelper attributeHelper = new AttributeHelper();
+
+        // Gets 'DisplayAttribute.Description' for the 'member'.
+        public static string GetDescription<T>(this T member)
+        {
+            bool success = attributeHelper.TryGetSpecifiedAttribute(member, out DisplayAttribute attribute);
+            if (success && !string.IsNullOrEmpty(attribute?.Description))
+                return attribute.Description;
+            return string.Empty;
+        }
+
+        // Gets 'DisplayAttribute.Name' for the 'member'.
+        public static string GetName<T>(this T member)
+        {
+            bool success = attributeHelper.TryGetSpecifiedAttribute(member, out DisplayAttribute attribute);
+            if (success && !string.IsNullOrEmpty(attribute?.Name))
+                return attribute.Name;
+            return string.Empty;
+        }
+
+        // Gets 'DisplayAttribute.ShortName' for the 'member'.
+        public static string GetShortName<T>(this T member)
+        {
+            bool success = attributeHelper.TryGetSpecifiedAttribute(member, out DisplayAttribute attribute);
+            if (success && !string.IsNullOrEmpty(attribute?.ShortName))
+                return attribute.ShortName;
+            return string.Empty;
+        }
+    }
+}
