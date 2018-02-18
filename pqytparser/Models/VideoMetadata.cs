ce1ed0c9ea597f8341fee08212acd26eb82e2166
pdcdeveloper <pdcdeveloper@outlook.com>
2018-02-18 09:29:43 -0800

    Edit 'VideoMetadata' (WIP)

--- pqytparser/Models/VideoMetadata.cs
+++ pqytparser/Models/VideoMetadata.cs
@@ -1,27 +1,57 @@
-﻿using pqytparser.Resources;
-using System;
+﻿using System;
+using pqytparser.Resources;
+using pqlib.Reflection.Attributes;
 
 namespace pqytparser.Models
 {
     public struct VideoMetadata
     {
-        public string ContentId { get; }
-        public string DownloadUrl { get; }
+        public readonly MediaQualityEnum Quality;
+        public readonly MimeTypeEnum MimeType;
+        public readonly string FileExt;
 
-        public MediaQualityEnum Quality { get; }
+        public string ContentId;
+        public string DownloadUrl;
 
-        public FileTypeEnum GetFileType()
+
+        public VideoMetadata(MediaQualityEnum quality)
         {
-            if (Quality == MediaQualityEnum.Unknown)
+            Quality = quality;
+            FileExt = Quality.GetShortName();
+            MimeType = Quality.GetMimeType();
+
+            ContentId = string.Empty;
+            DownloadUrl = string.Empty;
+        }
+
+        public VideoMetadata(string contentId, MediaQualityEnum quality) : this(quality)
+        {
+            ContentId = contentId;
+        }
+    }
+
+    // The static methods of this class should only ever be used by the 'VideoMetadata' model.
+    static class VideoMetaDataHelpers
+    {
+        public static FileTypeEnum GetFileType(this MediaQualityEnum quality)
+        {
+            if (quality == MediaQualityEnum.Unknown)
                 return FileTypeEnum.Unknown;
-            //todo
             foreach (FileTypeEnum ft in Enum.GetValues(typeof(FileTypeEnum)))
-                ;
+                if (quality.GetShortName().ToLower() == ft.GetShortName().ToLower())
+                    return ft;
             return FileTypeEnum.Unknown;
         }
 
-
-        //public FileTypeEnum FileExt { get; }
-        //public MimeTypeEnum MimeType { get; }
+        public static MimeTypeEnum GetMimeType(this MediaQualityEnum quality)
+        {
+            if (quality == MediaQualityEnum.Unknown)
+                return MimeTypeEnum.Unknown;
+            foreach (MimeTypeEnum mt in Enum.GetValues(typeof(MimeTypeEnum)))
+                if (quality.GetDescription().ToLower() == mt.GetDescription().ToLower())
+                    return mt;
+            return MimeTypeEnum.Unknown;
+        }
     }
+
 }
