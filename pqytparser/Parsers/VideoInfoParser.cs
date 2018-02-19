8bab53e03f5193f47db17d8504f9498b2de3df9b
pdcdeveloper <pdcdeveloper@outlook.com>
2018-02-19 14:23:42 -0800

    Add pqytparser.Parsers.VideoInfoParser

---
+++ pqytparser/Parsers/VideoInfoParser.cs
@@ -0,0 +1,29 @@
+﻿using pqytparser.Interfaces;
+using pqytparser.Models;
+using pqytparser.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pqytparser.Parsers
+{
+    public class VideoInfoParser : IParser
+    {
+        public Task<VideoDownloadInfo> GetVideoDownloadInfoAsync(string contentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetDesiredFileTypes(params FileTypeEnum[] fileTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetDesiredMimeTypes(params MimeTypeEnum[] mimeTypes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
