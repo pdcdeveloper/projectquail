17b09d8fc4b87cc4ac72508777980415161b8f86
pdcdeveloper <pdcdeveloper@outlook.com>
2018-02-19 14:48:40 -0800

    Edit add private static class to file to hold the helper methods and resources for VideoInfoParser

--- pqytparser/Parsers/VideoInfoParser.cs
+++ pqytparser/Parsers/VideoInfoParser.cs
@@ -26,4 +26,27 @@ namespace pqytparser.Parsers
             throw new NotImplementedException();
         }
     }
+
+
+    // Helper methods for VideoInfoParser.
+    static class VideoInfoParserHelpers
+    {
+        // Patterns use positive lookbehind and lazy modifier positive lookahead ("C# 5.0 In a Nutshell" by the Albahari brothers (p.998))
+        const string BasicItagPattern = @"itag=\d{1,3}";                // match "itag=ddd"   return "itag=ddd"
+        
+        
+        // Parses 'input' for the first instance of an query parameter "itag".
+        public static bool TryParseUrlForItag(string input, out string output)
+        {
+            const string ItagPattern1 = @"(?<=\x3F)itag=\d{1,3}?(?=\x26)";  // match "?itag=ddd&" return "itag=ddd"
+            const string ItagPattern2 = @"(?<=\x26)itag=\d{1,3}?(?=\x26)";  // match "&itag=ddd&" return "itag=ddd"
+            const string ItagPattern3 = @"(?<=\x26)itag=\d{1,3}";           // match "&itag=ddd"  return "itag=ddd"
+        }
+
+        // Safely cast from a string to 'Resources.MediaQualityEnum'.  Relies on the int value of each enum member within 'MediaQualityEnum'.
+        public static MediaQualityEnum MaptoItag(string input)
+        {
+            const string ItagValuePattern = @"(?<=itag=)\d{1,3}";           // match "itag=ddd"   return "ddd"
+        }
+    }
 }
