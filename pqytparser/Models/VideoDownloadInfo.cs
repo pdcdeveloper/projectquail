5ca79f73dc98bec6987e766bbdc3e7e20726387e
pdcdeveloper <pdcdeveloper@outlook.com>
2018-02-19 14:25:17 -0800

    Edit changed from struct to class

--- pqytparser/Models/VideoDownloadInfo.cs
+++ pqytparser/Models/VideoDownloadInfo.cs
@@ -2,9 +2,15 @@
 
 namespace pqytparser.Models
 {
-    public struct VideoDownloadInfo
+    public class VideoDownloadInfo
     {
         public VideoAvailability Availability { get; }
         public IList<VideoMetadata> Videos { get; }
+        public string ContentId { get; }
+
+        public VideoDownloadInfo(string contentId)
+        {
+            ContentId = contentId;
+        }
     }
 }
