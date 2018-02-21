8c6bd1242bf6a6e80da5c4d6994af8abf9ea5422
pdcdeveloper <pdcdeveloper@outlook.com>
2018-02-21 13:03:18 -0800

    Edit VideoInfoParser (WIP)

--- pqytparser/ViewModels/VideoInfoParser.cs
+++ pqytparser/ViewModels/VideoInfoParser.cs
@@ -6,13 +6,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Web.Http;
 
 namespace pqytparser.ViewModels
 {
     public class VideoInfoParser : IVideoInfoParser
     {
-        const string VideoInfoBaseUrl = "https://www.youtube.com/get_video_info?video_id=";
-
         public async Task<VideoDownloadInfo> GetContentUriAsync(string contentId, IList<MimeTypeEnum> mimeTypes, IList<FileTypeEnum> fileTypes)
         {
             if (string.IsNullOrEmpty(contentId))
@@ -34,7 +33,21 @@ namespace pqytparser.ViewModels
                 fileTypes.Add(FileTypeEnum.Webm);
             }
 
+            // Retrieve the DOM.
+            string dom = await GetVideoInfoDom(contentId);
+            
             //
+        }
+
+        async Task<string> GetVideoInfoDom(string contentId)
+        {
+            const string videoInfoBaseUrl = "https://www.youtube.com/get_video_info?video_id=";
+
+            Uri videoInfoUri = new Uri(videoInfoBaseUrl + contentId, UriKind.Absolute);
+            string response = string.Empty;
+            using (HttpClient client = new HttpClient())
+                response = await client.GetStringAsync(videoInfoUri);
+            return response;
         }
     }
 
@@ -42,6 +55,25 @@ namespace pqytparser.ViewModels
     // Helper methods for VideoInfoParser.
     static class VideoInfoParserHelpers
     {
+        // Keep the VideoInfoParser clean.
+        public const string responseError = "&errorcode";
+        public const string responseErrorCode = "&errorcode=150";
+        public const string responsePurchase = "&requires_purchase";
+        public const string https = "https";
+        public const string mime = "mime=";
+        public const string ytimg = "ytimg";
+        public const string url = "url=";
+
+        public const string streamMapPattern = "(?<=url_encoded_fmt_stream_map=).*";
+        public const string codecsPattern = "(?<=codecs=\x22.*?\x22).*";
+        public const string projectionPattern = "\x26projection_type=.*";
+        public const string clenPattern = @"\x26clen=\d*|clen=\d*";
+        public const string lmtPattern = @"\x26lmt=\d*|lmt=\d*";
+        public const string fallbackHostPattern = "fallback_host=.*\x26|fallback_Host=.*\x2C";
+        public const string itagPattern = @"\x26itag=\d{1,3}|itag=\d{1,3}";
+        public const string typePattern1 = "(?<=.*)type=.*\x22";
+        public const string typePattern2 = "\x26type=.*\x22";
+
         // Patterns use positive lookbehind and lazy modifier positive lookahead ("C# 5.0 In a Nutshell" by the Albahari brothers (p.998))
         const string BasicItagPattern = @"itag=\d{1,3}";                // match "itag=ddd"   return "itag=ddd"
         
