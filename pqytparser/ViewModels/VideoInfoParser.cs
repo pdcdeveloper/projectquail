5fe8d66e326997e7c58f3e1b8a5f6dcf4dd33b59
pdcdeveloper <pdcdeveloper@outlook.com>
2018-02-22 10:58:24 -0800

    Changed VideoInfoParser by removing the private static helper class within the file (methods were moved to ViewModels.VideoInfoDomDecoder)

--- pqytparser/ViewModels/VideoInfoParser.cs
+++ pqytparser/ViewModels/VideoInfoParser.cs
@@ -33,63 +33,7 @@ namespace pqytparser.ViewModels
                 fileTypes.Add(FileTypeEnum.Webm);
             }
 
-            // Retrieve the DOM.
-            string dom = await GetVideoInfoDom(contentId);
-            
-            //
-        }
-
-        async Task<string> GetVideoInfoDom(string contentId)
-        {
-            const string videoInfoBaseUrl = "https://www.youtube.com/get_video_info?video_id=";
-
-            Uri videoInfoUri = new Uri(videoInfoBaseUrl + contentId, UriKind.Absolute);
-            string response = string.Empty;
-            using (HttpClient client = new HttpClient())
-                response = await client.GetStringAsync(videoInfoUri);
-            return response;
-        }
-    }
-
-
-    // Helper methods for VideoInfoParser.
-    static class VideoInfoParserHelpers
-    {
-        // Keep the VideoInfoParser clean.
-        public const string responseError = "&errorcode";
-        public const string responseErrorCode = "&errorcode=150";
-        public const string responsePurchase = "&requires_purchase";
-        public const string https = "https";
-        public const string mime = "mime=";
-        public const string ytimg = "ytimg";
-        public const string url = "url=";
-
-        public const string streamMapPattern = "(?<=url_encoded_fmt_stream_map=).*";
-        public const string codecsPattern = "(?<=codecs=\x22.*?\x22).*";
-        public const string projectionPattern = "\x26projection_type=.*";
-        public const string clenPattern = @"\x26clen=\d*|clen=\d*";
-        public const string lmtPattern = @"\x26lmt=\d*|lmt=\d*";
-        public const string fallbackHostPattern = "fallback_host=.*\x26|fallback_Host=.*\x2C";
-        public const string itagPattern = @"\x26itag=\d{1,3}|itag=\d{1,3}";
-        public const string typePattern1 = "(?<=.*)type=.*\x22";
-        public const string typePattern2 = "\x26type=.*\x22";
-
-        // Patterns use positive lookbehind and lazy modifier positive lookahead ("C# 5.0 In a Nutshell" by the Albahari brothers (p.998))
-        const string BasicItagPattern = @"itag=\d{1,3}";                // match "itag=ddd"   return "itag=ddd"
-        
-        
-        // Parses 'input' for the first instance of an query parameter "itag".
-        public static bool TryParseUrlForItag(string input, out string output)
-        {
-            const string ItagPattern1 = @"(?<=\x3F)itag=\d{1,3}?(?=\x26)";  // match "?itag=ddd&" return "itag=ddd"
-            const string ItagPattern2 = @"(?<=\x26)itag=\d{1,3}?(?=\x26)";  // match "&itag=ddd&" return "itag=ddd"
-            const string ItagPattern3 = @"(?<=\x26)itag=\d{1,3}";           // match "&itag=ddd"  return "itag=ddd"
-        }
-
-        // Safely cast from a string to 'Resources.MediaQualityEnum'.  Relies on the int value of each enum member within 'MediaQualityEnum'.
-        public static MediaQualityEnum MaptoItag(string input)
-        {
-            const string ItagValuePattern = @"(?<=itag=)\d{1,3}";           // match "itag=ddd"   return "ddd"
+            // Parse
         }
     }
 }
