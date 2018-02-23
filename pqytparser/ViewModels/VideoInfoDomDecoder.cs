73d017ed975e126d173e4fb45072ff7d2dd8b24e
pdcdeveloper <pdcdeveloper@outlook.com>
2018-02-23 14:39:59 -0800

    Edit ViewModels.VideoInfoDomDecoder (WIP)

--- pqytparser/ViewModels/VideoInfoDomDecoder.cs
+++ pqytparser/ViewModels/VideoInfoDomDecoder.cs
@@ -14,11 +14,11 @@ namespace pqytparser.ViewModels
 {
     public class VideoInfoDomDecoder : IVideoInfoDomDecoder
     {
-        // Patterns use positive lookbehind and lazy modifier positive lookahead ("C# 5.0 In a Nutshell" by the Albahari brothers (p.998))
+        // Patterns use positive lookbehind and lazy modifier positive lookahead ("C# 5.0 In a Nutshell" by the Albahari brothers (p.998)).
         const string basicItagPattern = @"itag=\d{1,3}";                // match "itag=ddd"   return "itag=ddd"
 
 
-        // Retrieves the available download urls by decoding the video info dom for the 'contentId' to parse for itags.
+        // Retrieves the available download urls by decoding the video info DOM for the 'contentId' to parse for itags.
         public VideoDownloadInfo GetVideoDownloadInfo(string contentId, string contentTitle, string dom)
         {
             const string responseError = "&errorcode";
@@ -30,14 +30,6 @@ namespace pqytparser.ViewModels
             const string url = "url=";
 
             const string streamMapPattern = "(?<=url_encoded_fmt_stream_map=).*";
-            const string codecsPattern = "(?<=codecs=\x22.*?\x22).*";
-            const string projectionPattern = "\x26projection_type=.*";
-            const string clenPattern = @"\x26clen=\d*|clen=\d*";
-            const string lmtPattern = @"\x26lmt=\d*|lmt=\d*";
-            const string fallbackHostPattern = "fallback_host=.*\x26|fallback_Host=.*\x2C";
-            const string itagPattern = @"\x26itag=\d{1,3}|itag=\d{1,3}";
-            const string typePattern1 = "(?<=.*)type=.*\x22";
-            const string typePattern2 = "\x26type=.*\x22";
 
 
             if (string.IsNullOrEmpty(contentId))
@@ -47,25 +39,45 @@ namespace pqytparser.ViewModels
             if (string.IsNullOrEmpty(dom))
                 return new VideoDownloadInfo(contentId, contentTitle, new VideoAvailability(VideoAvailabilityEnum.NotAvailable, "Missing dom."), null);
 
-            // Decode pass 1
+            // Decode pass 1.
             dom = WebUtility.UrlDecode(dom);
 
             // Check for errors and specific messages.
             if (dom.Contains(responseError) || dom.Contains(responsePurchase))
             {
+                string errorMessage = string.Empty;
+                VideoAvailabilityEnum errorStatus = VideoAvailabilityEnum.NotAvailable;
+
                 if (dom.Contains(responseErrorCode))
                 {
-                    
+                    errorMessage = "YouTube video info response contains errorcode 150.";
+                    errorStatus = VideoAvailabilityEnum.ErrorCode;
                 }
                 else if (responseError.Contains(responsePurchase))
                 {
-
+                    errorMessage = "YouTube content requires purchase.";
+                    errorStatus = VideoAvailabilityEnum.RequiresPurchase;
                 }
                 else
                 {
-                    return new VideoDownloadInfo(contentId, contentTitle, new VideoAvailability(VideoAvailabilityEnum.NotAvailable, "The dom contains an error in the response."), null);
+                    errorMessage = "The dom contains an error in the response.";
+                    errorStatus = VideoAvailabilityEnum.ErrorCode;
                 }
+
+                return new VideoDownloadInfo(contentId, contentTitle, new VideoAvailability(errorStatus, errorMessage), null);
             }
+
+            // Get everything after "url_encoded_fmt_stream_map=".
+            dom = Regex.Match(dom, streamMapPattern).Value;
+
+            // Decode pass 2.
+            dom = WebUtility.UrlDecode(dom);
+
+            // Split.
+            string[] responseUrls = Regex.Split(dom, url);
+
+            // Clean up the URLs.
+
         }
 
 
@@ -82,12 +94,84 @@ namespace pqytparser.ViewModels
         }
 
 
+        // Cleans up a URL by removing unnecessary query parameters.
+        // <see cref="https://stackoverflow.com/questions/73883/string-vs-stringbuilder"/>
+        // <see cref="https://support.microsoft.com/en-us/help/306822/how-to-improve-string-concatenation-performance-in-visual-c"/>
+        string RemoveQueryParameters(string input)
+        {
+            const string codecsPattern = "(?<=codecs=\x22.*?\x22).*";
+            const string projectionPattern = "\x26projection_type=.*";
+            const string clenPattern = @"\x26clen=\d*|clen=\d*";
+            const string lmtPattern = @"\x26lmt=\d*|lmt=\d*";
+            const string fallbackHostPattern = "fallback_host=.*\x26|fallback_Host=.*\x2C";
+            const string itagPattern = @"\x26itag=\d{1,3}|itag=\d{1,3}";
+            const string typePattern1 = "(?<=.*)type=.*\x22";
+            const string typePattern2 = "\x26type=.*\x22";
+
+
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            string url = new string(input.ToCharArray());
+
+            // Decode.  By the time the 'url' gets here, this should be pass 3.
+            url = WebUtility.UrlDecode(url);
+
+            // Remove everything after the "codecs" parameter.
+            Match mcodecsp = Regex.Match(url, codecsPattern);
+            if (mcodecsp.Success)
+                url = url.Remove(mcodecsp.Index, mcodecsp.Length);
+
+            // Remove "projection_type" parameter and everything after it.
+            Match mprojectionp = Regex.Match(url, projectionPattern);
+            if (mprojectionp.Success)
+                url = url.Remove(mprojectionp.Index, mprojectionp.Length);
+
+            // Remove the trailing "clen" parameter.
+            MatchCollection mclenp = Regex.Matches(url, clenPattern, RegexOptions.RightToLeft);
+            if (mclenp.Count > 1)
+                url = url.Remove(mclenp[0].Index, mclenp[0].Length);
+
+            // Remove the trailing "lmt" pattern.
+            MatchCollection mlmtp = Regex.Matches(url, lmtPattern, RegexOptions.RightToLeft);
+            if (mlmtp.Count > 1)
+                url = url.Remove(mlmtp[0].Index, mlmtp[0].Length);
+
+            // Remove "fallback_host" parameter.
+            Match mfallbackhostp = Regex.Match(url, fallbackHostPattern);
+            if (mfallbackhostp.Success)
+                url = url.Remove(mfallbackhostp.Index, mfallbackhostp.Length);
+
+            // Remove trailing "itag" parameter.  Sometimes there's multiple itags within a url -- there can only be one.
+            MatchCollection mitagp = Regex.Matches(url, itagPattern, RegexOptions.RightToLeft);
+            if (mitagp.Count > 1)
+                url = url.Remove(mitagp[0].Index, mitagp[0].Length);
+
+            // Sometimes the "&" infront of the "type" parameter is no longer there and it must be manually re-added.
+            Match mtypep1 = Regex.Match(url, typePattern1);
+            if (mtypep1.Success)
+            {
+                Match mtypep2 = Regex.Match(url, typePattern2);
+                if (!mtypep2.Success)
+                {
+                    // Remove the match first.
+                    url = url.Remove(mtypep1.Index, mtypep1.Length);
+                    // Append an "&" followed by the "type" parameter from the match.
+                    url = url.Insert(mtypep1.Index, '\x26' + mtypep1.Value);
+                }
+            }
+
+            return url;
+        }
+
+
         // Parses 'input' for the first instance of an query parameter "itag=ddd".
         bool TryParseUrlForItag(string input, out string output)
         {
             const string itagPattern1 = @"(?<=\x3F)itag=\d{1,3}?(?=\x26)";  // match "?itag=ddd&" return "itag=ddd"
             const string itagPattern2 = @"(?<=\x26)itag=\d{1,3}?(?=\x26)";  // match "&itag=ddd&" return "itag=ddd"
             const string itagPattern3 = @"(?<=\x26)itag=\d{1,3}";           // match "&itag=ddd"  return "itag=ddd"
+
 
             output = null;
 
