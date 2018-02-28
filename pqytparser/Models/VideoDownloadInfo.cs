1e9c3ec95fff10f47cda210e14df7b210f407336
pdcdeveloper <pdcdeveloper@outlook.com>
2018-02-27 16:56:16 -0800

    Edit Models.VideoDownloadInfo field and parameter name change

--- pqytparser/Models/VideoDownloadInfo.cs
+++ pqytparser/Models/VideoDownloadInfo.cs
@@ -8,14 +8,14 @@ namespace pqytparser.Models
         public readonly string ContentId;
         public readonly string ContentTitle;
 
-        public readonly VideoAvailability Result;
+        public readonly VideoAvailability Status;
         public readonly List<VideoMetadata> Videos;
 
-        public VideoDownloadInfo(string contentId, string contentTitle, VideoAvailability parserResult, List<VideoMetadata> videos)
+        public VideoDownloadInfo(string contentId, string contentTitle, VideoAvailability status, List<VideoMetadata> videos)
         {
             ContentId = contentId;
             ContentTitle = contentTitle;
-            Result = parserResult;
+            Status = status;
             Videos = (videos?.Count ?? 0) > 0 ? new List<VideoMetadata>(videos) : null;
         }
     }
