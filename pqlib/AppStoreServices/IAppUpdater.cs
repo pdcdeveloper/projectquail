ee2e09c09bde9e5e6aef2c88b3b626b3076ead36
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-06 05:59:18 -0800

    Added 'pqlib.AppStoreServices' namespace

---
+++ pqlib/AppStoreServices/IAppUpdater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pqlib.AppStoreServices
+{
+    public interface IAppUpdater
+    {
+        Task<bool> DownloadAndInstallMostRecentVersionAsync();
+    }
+}
