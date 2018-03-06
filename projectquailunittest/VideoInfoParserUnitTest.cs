ad72736dee4717d1e66727ef86a56771fb9ab42d
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-05 19:24:30 -0800

    Added 'projectquailunittest.VideoInfoParserUnitTest'

---
+++ projectquailunittest/VideoInfoParserUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pqytparser.Interfaces;
+using pqytparser.Resources;
+using pqytparser.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectquailunittest
+{
+    [TestClass]
+    public class VideoInfoParserUnitTest
+    {
+        IVideoInfoParser _videoInfoParser = new VideoInfoParser();
+
+
+        [TestMethod]
+        public async Task GetContentUriAsync_DynamicTest()
+        {
+            // "Monster Hunter World" by videogamedunkey.
+            IList<MimeTypeEnum> mimeTypes = new List<MimeTypeEnum>() { MimeTypeEnum.Audio, MimeTypeEnum.Video, MimeTypeEnum.MuxedAudioVideo };
+            IList<FileTypeEnum> fileTypes = new List<FileTypeEnum>() { FileTypeEnum.M4a, FileTypeEnum.Mp4, FileTypeEnum.Webm };
+
+            var info = await _videoInfoParser.GetContentUriAsync("Mxv9AM397Y8", "Monster Hunter World", mimeTypes, fileTypes);
+
+            if (info.Availability == VideoAvailabilityEnum.NotAvailable)
+                Assert.IsTrue(info.ErrorMessage.ToLower().Contains("not found"));
+            else
+                Assert.IsTrue(info.Availability == VideoAvailabilityEnum.Available);
+        }
+    }
+}
