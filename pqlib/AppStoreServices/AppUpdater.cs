503832c2cd94d483ba88fa6e3808c751e251d606
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-08 02:21:06 -0800

    Edit AppStoreServices.AppUpdater.DownloadAndInstallMostRecentVersionAsync() (WIP)

--- pqlib/AppStoreServices/AppUpdater.cs
+++ pqlib/AppStoreServices/AppUpdater.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Services.Store;
+using Windows.UI.Popups;
 
 namespace pqlib.AppStoreServices
 {
@@ -19,9 +20,47 @@ namespace pqlib.AppStoreServices
             _storeContext = storeContext ?? StoreContext.GetDefault();
         }
 
-        public Task<bool> DownloadAndInstallMostRecentVersionAsync()
+        public async Task<bool> DownloadAndInstallMostRecentVersionAsync()
         {
-            throw new NotImplementedException();
+            if (_storeContext == null)
+            {
+                return false;
+            }
+
+            // Get available updates.
+            var updates = await _storeContext.GetAppAndOptionalStorePackageUpdatesAsync();
+            if ((updates?.Count ?? 0) < 1)
+            {
+                return false;   // No updates were found
+            }
+
+            // Content verification.
+            foreach (var update in updates)
+                if (!await update.Package.VerifyContentIntegrityAsync())
+                    return false;
+
+            // Download and install.
+            // This method will automatically show a consent dialog.
+            var downloadOperation = _storeContext.RequestDownloadAndInstallStorePackageUpdatesAsync(updates);
+
+            // Show the message dialog.
+            var result = await downloadOperation.AsTask();
+            switch (result.OverallState)
+            {
+                case StorePackageUpdateState.Completed:
+                    return true;
+
+                case StorePackageUpdateState.Canceled:
+                    break;
+
+                case StorePackageUpdateState.OtherError:
+                    MessageDialog mdialog = new MessageDialog("An app update was detected, but may still be unavailable to download.  Please try updating again at a later time.");
+                    mdialog.Commands.Add(new UICommand("Close this dialog"));
+                    IUICommand command = await mdialog.ShowAsync();
+                    break;
+            }
+
+            return false;   // Something went wrong
         }
     }
 }
