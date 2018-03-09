05154211ff4c2758ee31554eca313c7733ee1e00
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-09 13:40:38 -0800

    Added 'projectquailunittest.PersistentGuidUnitTest'

---
+++ projectquailunittest/PersistentGuidUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pqappdatastorage.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectquailunittest
+{
+    [TestClass]
+    public class PersistentGuidUnitTest
+    {
+        [TestMethod]
+        public void PersistentGuid_BasicTest()
+        {
+            PersistentGuid pGuid = new PersistentGuid();
+            Assert.IsTrue(pGuid.AppGuid != Guid.Empty);
+        }
+    }
+}
