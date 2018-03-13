97bd8e462762647b5228550996a5e791f43ea011
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-13 05:50:02 -0700

    Added 'projectquailunittest.SolidColorBrushUnitTest' along with 'Microsoft.Toolkit.Uwp' NuGet package for its 'ColorHelper' class

---
+++ projectquailunittest/SolidColorBrushUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Toolkit.Uwp.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace projectquailunittest
+{
+    [TestClass]
+    public class SolidColorBrushUnitTest
+    {
+        [TestMethod]
+        public void SolidColorBrush_FromStringTest()
+        {
+            var color = "PeachPuff".ToColor();
+            Assert.IsTrue(color == Colors.PeachPuff);
+
+            var hexColor = "#FF171717";
+            color = "#FF171717".ToColor();
+            Assert.IsTrue(color == Color.FromArgb(
+                byte.Parse(hexColor.Substring(1, 2), NumberStyles.HexNumber),
+                byte.Parse(hexColor.Substring(3, 2), NumberStyles.HexNumber),
+                byte.Parse(hexColor.Substring(5, 2), NumberStyles.HexNumber),
+                byte.Parse(hexColor.Substring(7, 2), NumberStyles.HexNumber)));
+        }
+    }
+}
