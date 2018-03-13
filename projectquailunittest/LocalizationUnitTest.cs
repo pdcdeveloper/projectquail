8cea217d6cbcaa6d0e0221df5dae14e378b9bc5c
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-12 21:39:43 -0700

    Added 'projectquailunittest.LocalizationUnitTest' class

---
+++ projectquailunittest/LocalizationUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+
+namespace projectquailunittest
+{
+    [TestClass]
+    public class LocalizationUnitTest
+    {
+        ResourceLoader _resourceLoader = ResourceLoader.GetForCurrentView();
+
+        [TestMethod]
+        public void ResourcesEnglish_Test()
+        {
+            string greeting = _resourceLoader.GetString("Greeting");
+            Assert.IsTrue(!string.IsNullOrEmpty(greeting));
+            Assert.IsTrue(greeting == "Hello, World!");
+        }
+
+        [TestMethod]
+        public void ResourcesLatin_Test()
+        {
+
+        }
+    }
+}
