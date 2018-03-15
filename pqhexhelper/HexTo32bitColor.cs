04212b54ff9cef6ab1b97ee276e00369fcffe7a2
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-15 08:19:46 -0700

    Edit 'pqhexhelper.HexTo32bitColor' (needs unit testing)

--- pqhexhelper/HexTo32bitColor.cs
+++ pqhexhelper/HexTo32bitColor.cs
@@ -11,62 +11,100 @@ namespace pqhexhelper
     // Provides methods to parse hex strings (syntax "#FFFFFFFF" or "FFFFFFFF") into 'Windows.UI.Color' or into separate argb byte values.
     public static class HexTo32bitColor
     {
-        public static (byte alpha, byte red, byte green, byte blue) GetArgb(this string hex)
-        {
-            (byte a, byte r, byte g, byte b) argb = (Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue);
-
-            return argb;
-        }
-        
         public static bool TryGetArgb(this string hex, out (byte alpha, byte red, byte green, byte blue) argb)
         {
             argb = (Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue);
 
-            return false;
+            if (!TryGetValidateStartingIndex(hex, out int startingIndex))
+                return false;
+
+            //if (!Byte.TryParse(hex.Substring(startingIndex + 0, 2), NumberStyles.HexNumber, null, out argb.alpha))
+            //    return false;
+            //if (!Byte.TryParse(hex.Substring(startingIndex + 2, 2), NumberStyles.HexNumber, null, out argb.red))
+            //    return false;
+            //if (!Byte.TryParse(hex.Substring(startingIndex + 4, 2), NumberStyles.HexNumber, null, out argb.green))
+            //    return false;
+            //if (!Byte.TryParse(hex.Substring(startingIndex + 6, 2), NumberStyles.HexNumber, null, out argb.blue))
+            //    return false;
+
+            argb.alpha = Byte.Parse(hex.Substring(startingIndex + 0, 2), NumberStyles.HexNumber);
+            argb.red = Byte.Parse(hex.Substring(startingIndex + 2, 2), NumberStyles.HexNumber);
+            argb.green = Byte.Parse(hex.Substring(startingIndex + 4, 2), NumberStyles.HexNumber);
+            argb.blue = Byte.Parse(hex.Substring(startingIndex + 6, 2), NumberStyles.HexNumber);
+
+            return true;
         }
 
-        public static byte GetAlphaValue(this string hex)
-        {
-            return Byte.MinValue;
-        }
-
-        public static bool TryGetAlphaValue(this string hex, out byte alpha)
+        public static bool TryGetAlpha(this string hex, out byte alpha)
         {
             alpha = Byte.MinValue;
-
             if (TryGetValidateStartingIndex(hex, out int startingIndex))
                 if (Byte.TryParse(hex.Substring(startingIndex + 0, 2), NumberStyles.HexNumber, null, out alpha))
                     return true;
             return false;
         }
 
-        public static byte GetRedValue(this string hex)
-        {
-            return byte.MinValue;
-        }
-
-        public static bool TryGetRedValue(this string hex, out byte red)
+        public static bool TryGetRed(this string hex, out byte red)
         {
             red = Byte.MinValue;
-            
+            if (TryGetValidateStartingIndex(hex, out int startingIndex))
+                if (Byte.TryParse(hex.Substring(startingIndex + 2, 2), NumberStyles.HexNumber, null, out red))
+                    return true;
             return false;
         }
-
-        public static bool TryGetGreenValue(this string hex, out byte green)
+        
+        public static bool TryGetGreen(this string hex, out byte green)
         {
             green = Byte.MinValue;
-            
-
+            if (TryGetValidateStartingIndex(hex, out int startingIndex))
+                if (Byte.TryParse(hex.Substring(startingIndex + 4, 2), NumberStyles.HexNumber, null, out green))
+                    return true;
             return false;
         }
 
-        public static bool TryGetBlueValue(this string hex, out byte blue)
+        public static bool TryGetBlue(this string hex, out byte blue)
         {
             blue = Byte.MinValue;
-
+            if (TryGetValidateStartingIndex(hex, out int startingIndex))
+                if (Byte.TryParse(hex.Substring(startingIndex + 6, 2), NumberStyles.HexNumber, null, out blue))
+                    return true;
             return false;
         }
 
+        public static (byte Alpha, byte Red, byte Green, byte Blue) GetArgb(this string hex)
+        {
+            if (TryGetArgb(hex, out (byte, byte, byte, byte) argb))
+                return argb;
+            return (Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue);
+        }
+
+        public static byte GetAlpha(this string hex)
+        {
+            if (TryGetAlpha(hex, out byte alpha))
+                return alpha;
+            return Byte.MinValue;
+        }
+
+        public static byte GetRed(this string hex)
+        {
+            if (TryGetRed(hex, out byte red))
+                return red;
+            return byte.MinValue;
+        }
+        public static byte GetGreen(this string hex)
+        {
+            if (TryGetGreen(hex, out byte green))
+                return green;
+            return Byte.MinValue;
+        }
+
+        public static byte GetBlue(this string hex)
+        {
+            if (TryGetBlue(hex, out byte blue))
+                return blue;
+            return Byte.MinValue;
+        }
+        
         public static bool TryGetValidateStartingIndex(this string hex, out int startingIndex)
         {
             startingIndex = -1;
