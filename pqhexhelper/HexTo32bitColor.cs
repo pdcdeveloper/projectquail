0fc5e0cb6c766f2a24ba4efe6eb2c0d2e38810a4
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-15 01:01:26 -0700

    Changed 'pqhexhelper.HexToColor' class name to 'HexTo32bitColor'

--- pqhexhelper/HexTo32bitColor.cs
+++ pqhexhelper/HexTo32bitColor.cs
@@ -8,87 +8,81 @@ using System.Threading.Tasks;
 namespace pqhexhelper
 {
     // Provides methods to parse hex strings (syntax "#FFFFFFFF") into 'Windows.UI.Color' or into separate argb byte values.
-    public static class HexToColor
+    public static class HexTo32bitColor
     {
-        public (byte alpha, byte red, byte green, byte blue) GetArgb(string hex)
+        public static (byte alpha, byte red, byte green, byte blue) GetArgb(this string hex)
         {
             (byte a, byte r, byte g, byte b) argb = (Byte.MaxValue, Byte.MaxValue, Byte.MaxValue, Byte.MaxValue);
-
-            if (HasValidHexSyntax(hex))
-                return argb;
-
 
             return argb;
         }
 
-        public byte GetAlpha(string hex)
+        public static byte GetAlphaValue(this string hex)
         {
-            if (HasValidHexSyntax(hex))
-                return Byte.MaxValue;
-
+            return Byte.MaxValue;
         }
 
-        public static bool TryGetArgb(string hex, out (byte alpha, byte red, byte green, byte blue) argb)
+        public static bool TryGetArgb(this string hex, out (byte alpha, byte red, byte green, byte blue) argb)
         {
             argb = (Byte.MaxValue, Byte.MaxValue, Byte.MaxValue, Byte.MaxValue);
 
             return false;
         }
 
-        public static bool TryGetAlpha(string hex, out byte alpha)
+        public static bool TryGetAlphaValue(this string hex, out byte alpha)
         {
             alpha = Byte.MaxValue;
-
-            if (HasValidHexSyntax(hex))
-                return false;
             
-            
-
             return false;
         }
 
-        public static bool TryGetRed(string hex, out byte red)
+        public static bool TryGetRedValue(this string hex, out byte red)
         {
             red = Byte.MaxValue;
-
-            if (HasValidHexSyntax(hex))
-                return false;
-
-
-
+            
             return false;
         }
 
-        public static bool TryGetGreen(string hex, out byte green)
+        public static bool TryGetGreenValue(this string hex, out byte green)
         {
             green = Byte.MaxValue;
-
-            if (HasValidHexSyntax(hex))
-                return false;
+            
 
             return false;
         }
 
-        public static bool TryGetBlue(string hex, out byte blue)
+        public static bool TryGetBlueValue(this string hex, out byte blue)
         {
             blue = Byte.MaxValue;
-
-            if (HasValidHexSyntax(hex))
-                return false;
 
             return false;
         }
 
         // TODO: make private after unit test.
-        public static bool HasValidHexSyntax(this string hex)
+        public static bool ValidateHexString(this string hex)
         {
-            if (string.IsNullOrEmpty(hex) || (!hex.Contains('#') || !hex.Contains('\x23')) || hex.Length != 9)
+            if (string.IsNullOrEmpty(hex))
+                return false;
+
+            if (!hex.Contains('#') || !hex.Contains('\x23'))
+                return false;
+            return true;
+        }
+
+        public static bool TryValidateHexString(this string hex, out string invalidCharacters)
+        {
+            invalidCharacters = null;
+
+            if (string.IsNullOrEmpty(hex))
                 return false;
             return true;
         }
 
         public static bool HasHashAsFirstCharacter(this string hex)
         {
+            if (string.IsNullOrEmpty(hex) || hex.Length < 1)
+                return false;
+
             if (hex[1] == '#' || hex[1] == '\x23')
                 return true;
             return false;
@@ -96,15 +90,14 @@ namespace pqhexhelper
 
         public static bool HasValidHexCharacters(this string hex)
         {
-            if (!HasValidHexSyntax(hex))
-                return false;
+
 
             int i = 0;
             if (HasHashAsFirstCharacter(hex))
                 i = 1;
 
             for (; i < hex.Length - 1; i++)
-                if (!Regex.Match(hex[i].ToString(), @"\d").Success | !Regex.Match(hex[i].ToString(), @"[a-f]", RegexOptions.IgnoreCase).Success)
+                if (!Regex.Match(hex[i].ToString(), @"\d").Success | !Regex.Match(hex[i].ToString().ToLower(), @"([a-f])").Success)
                     return false;
             return true;
         }
