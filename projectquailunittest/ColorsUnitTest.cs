105360f143f9484212a941b66fc0bfa9502be496
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-19 14:23:59 -0700

    Added 'projectquailunittest.ColorsUnitTest.ColorsToHex_Test' to convert each 'Windows.UI.Colors' 'Windows.UI.Color' member into its hex representation

---
+++ projectquailunittest/ColorsUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Toolkit.Uwp.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.UI;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace projectquailunittest
+{
+    [TestClass]
+    public class ColorsUnitTest
+    {
+        [TestMethod]
+        public void ColorsToHex_Test()
+        {
+            MemberInfo[] members = typeof(Colors).GetMembers();
+            foreach (var member in members)
+                if (member.DeclaringType == typeof(Colors) && member.MemberType == MemberTypes.Property)
+                    Debug.WriteLine(member.Name + " == " + member.Name.ToColor().ToHex());
+        }
+    }
+}
