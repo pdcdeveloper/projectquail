f19a83648af95a149e53525d9e04dfe8309e7dc5
pdcdeveloper <pdcdeveloper@outlook.com>
2018-03-29 20:07:49 -0700

    Added 'pqlocalization.Models' namespace and added 'pqlocalization.Models.AppVersionInfo.cs'

---
+++ pqlocalization/Models/AppVersionInfo.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace pqlocalization.Models
+{
+    public struct AppVersionInfo
+    {
+        [JsonProperty(nameof(AppName))]
+        public string AppName { get; set; }
+
+        [JsonProperty(nameof(DevelopmentTeam))]
+        public string DevelopmentTeam { get; set; }
+
+        [JsonProperty(nameof(CurrentVersion))]
+        public PatchInfo CurrentVersion { get; set; }
+
+        [JsonProperty(nameof(PreviousVersions))]
+        public PatchInfo[] PreviousVersions { get; set; }
+    }
+
+    public struct PatchInfo
+    {
+        // "Major.Minor.Build.Revision" or "Major.Minor.MajorPatch.MinorPatch".
+        [JsonProperty(nameof(Version))]
+        public string Version { get; set; }
+
+        // "MMMM d, yyyy"
+        [JsonProperty(nameof(PublishedAt))]
+        public string PublishedAt { get; set; }
+
+        [JsonProperty(nameof(Changes))]
+        public PatchNote[] Changes { get; set; }
+    }
+
+    public struct PatchNote
+    {
+        [JsonProperty(nameof(Description))]
+        public string Description { get; set; }
+
+        [JsonProperty(nameof(Details))]
+        public string[] Details { get; set; }
+    }
+}
