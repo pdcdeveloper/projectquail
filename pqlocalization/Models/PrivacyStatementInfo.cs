93171a6c03d845af92a32a84e2dd37f4af83a149
pdcdeveloper <pdcdeveloper@outlook.com>
2018-04-03 02:37:28 -0700

    Added 'Models.PrivacyStatementInfo'

---
+++ pqlocalization/Models/PrivacyStatementInfo.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pqlocalization.Models
+{
+    public class PrivacyStatementInfo
+    {
+        [JsonProperty(nameof(PrivacyStatement))]
+        public string PrivacyStatement { get; set; }
+    }
+}
