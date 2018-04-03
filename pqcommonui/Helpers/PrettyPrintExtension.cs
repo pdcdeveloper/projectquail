a3d04cb2bd6f65eb697d095907e6c2d5977e2c2d
pdcdeveloper <pdcdeveloper@outlook.com>
2018-04-03 03:23:11 -0700

    Added 'Helpers.PrettyPrintExtension' (WIP)

---
+++ pqcommonui/Helpers/PrettyPrintExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pqcommonui.Helpers
+{
+    public static class PrettyPrintExtension
+    {
+        const string Epoch = "January 1, 1970";
+        const string NoValueIndicator = "\xC4\xC4"; // double em dash
+        const string LongDateFormatPattern = "MMMM d, yyyy";
+        const string CommaDelineatedPattern = "###,###,###,###";
+        const string DotDelineatedPattern = "###.###.###.###";
+
+
+    }
+}
