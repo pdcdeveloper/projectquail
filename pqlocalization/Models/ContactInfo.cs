58988026601934258c611c3857bbf74d335e804d
pdcdeveloper <pdcdeveloper@outlook.com>
2018-04-03 02:35:11 -0700

    Added 'Models.ContactInfo'

---
+++ pqlocalization/Models/ContactInfo.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace pqlocalization.Models
+{
+    public struct ContactInfo
+    {
+        [JsonProperty(nameof(DeveloperName))]
+        public string DeveloperName { get; set; }
+
+        [JsonProperty(nameof(Email))]
+        public string Email { get; set; }
+    }
+}
